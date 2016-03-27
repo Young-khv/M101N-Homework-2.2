88dd685b20d4c6972d75be70cb7a82c85b045920
Arkadiy Parinov <arkadiy.parinov@yandex.ru>
2016-03-27 18:29:07 +1000

    start 1.1

---
+++ M101N_Homework2-2/M101N_Homework2-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M101N_Homework2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(123);
+        }
+    }
+}
