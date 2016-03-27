b52aa5d3baeeac87fa4c0bf3f9696ab98e151503
Arkadiy Parinov <arkadiy.parinov@yandex.ru>
2016-03-27 20:03:12 +1000

    Complite hometask 2.2

--- M101N_Homework2-2/M101N_Homework2-2/Program.cs
+++ M101N_Homework2-2/M101N_Homework2-2/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
 
 namespace M101N_Homework2_2
 {
@@ -10,7 +13,39 @@ namespace M101N_Homework2_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(123);
+            MainAsync(args).GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            const string conString = "mongodb://localhost:27017";
+
+            var client = new MongoClient(conString);
+
+            var db = client.GetDatabase("students");
+
+            var collection = db.GetCollection<BsonDocument>("grades");
+
+            var builder = Builders<BsonDocument>.Filter;
+
+            var sort = Builders<BsonDocument>.Sort.Ascending("student_id").Ascending("score");
+
+            var filter = builder.Eq("type", "homework");
+            
+            var result = await collection.Find(filter)
+                .Sort(sort)
+                .ToListAsync();
+            var st_id = -1;
+            foreach (var student in result)
+            {
+                if (student["student_id"] != st_id)
+                {
+                    st_id = (int)student["student_id"];
+                    await collection.DeleteOneAsync(student);
+                    Console.WriteLine(st_id);
+                }
+            }
         }
     }
 }
